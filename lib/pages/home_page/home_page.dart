23c952aacc7419c91168e0c7bc55ca7a8a151a68
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-10-12 15:01:48 +0200

    Add nav drawer

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -8,8 +8,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: Text('TinX dashboard')),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('Poland can into space!'),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primary.withOpacity(0.5),
+              ),
+            ),
+            ListTile(title: Text('Settings')),
+          ],
+        ),
+      ),
       body: Center(
         child: Container(
           child: Text('Main page'),
